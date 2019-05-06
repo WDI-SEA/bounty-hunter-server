548ada33c19345d617ad8e80da6e56328911e07a
brandiw <brandimichelle.0911@gmail.com>
2019-05-06 16:16:48 -0700

    More stub routes

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -4,7 +4,29 @@ let express = require('express')
 // Router instance
 let router = express.Router()
 
-// Routes
+// GET /bounties
 router.get('/', (req, res) => {
   res.send('stub')
 })
+
+// POST /bounties
+router.post('/', (req, res) => {
+  res.send('stub')
+})
+
+// GET /bounties/:id
+router.get('/:id', (req, res) => {
+  res.send('stub')
+})
+
+// PUT /bounties/:id
+router.put('/:id', (req, res) => {
+  res.send('stub')
+})
+
+// DELETE /bounties/:id
+router.delete('/:id', (req, res) => {
+  res.send('stub')
+})
+
+module.exports = router
