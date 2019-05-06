21730fad0e3cad9e0be088219339f98701aba466
brandiw <brandimichelle.0911@gmail.com>
2019-05-06 16:11:03 -0700

    Stubbed routes for bounties

---
+++ controllers/v1/bounties.js
@@ -0,0 +1,10 @@
+// Required modules
+let express = require('express')
+
+// Router instance
+let router = express.Router()
+
+// Routes
+router.get('/', (req, res) => {
+  res.send('stub')
+})
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+// Required modules
 let express = require('express')
 
+// Express instance
 let app = express()
 
+// Controllers
+app.use('/v1/bounties', require('./controllers/v1/bounties'))
+
+// Catch-all (wildcard) route
 app.get('*', (req, res) => {
-  res.send('Hello world')
+  res.status(404).send('Not Found')
 })
 
+// Attach to port
 app.listen(process.env.PORT || 3000)
